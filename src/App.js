bff35b5a4c44bba672fd1e48a63537656308d4d4
Ryan Yost <ryost217@gmail.com>
2018-01-21 11:32:53 -0600

    social buttons

--- src/App.js
+++ src/App.js
@@ -21,13 +21,50 @@ class App extends Component {
           <p className="App-intro">Replace the React icon with a dog!</p>
         )}
 
-        {error && <p style={{ color: "red" }}>Uh oh - something went wrong!</p>}
-
         {fetching ? (
           <button disabled>Fetching...</button>
         ) : (
           <button onClick={onRequestDog}>Request a Dog</button>
         )}
+
+        {error && <p style={{ color: "red" }}>Uh oh - something went wrong!</p>}
+
+        <div style={{ display: "block", padding: "50px 0px 10px 0px" }}>
+          <a
+            style={{ padding: "0px 5%" }}
+            class="github-button"
+            href="https://github.com/ryanjyost/redux-saga-beginner-tutorial"
+            data-icon="octicon-star"
+            data-show-count="true"
+            aria-label="Star ryanjyost/dom-events on GitHub"
+          >
+            Star
+          </a>
+        </div>
+
+        <div style={{ display: "block", padding: "10px 5%" }}>
+          <a
+            style={{ padding: "0px 5%" }}
+            href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+            class="twitter-share-button"
+            data-text="Check out Dog Saga - A Redx-Saga Beginner Tutorial"
+            data-url="https://ryanjyost.github.io/redux-saga-beginner-tutorial/"
+            data-related="ryanjyost"
+            data-show-count="false"
+          >
+            Tweet
+          </a>
+        </div>
+        <div style={{ display: "block", padding: "10px 5%" }}>
+          <a
+            style={{ padding: "0px 5%" }}
+            href="https://twitter.com/ryanjyost?ref_src=twsrc%5Etfw"
+            class="twitter-follow-button"
+            data-show-count="false"
+          >
+            Follow @ryanjyost
+          </a>
+        </div>
       </div>
     );
   }
